a3dd2d4ddc64bc50ee0f899c101bc965dcd35407
artkis74 <kiselev_artem1991@mail.ru>
2022-07-16 12:52:16 +0500

    add new methods for Student and Lecturer

--- HW_OOP/main.py
+++ HW_OOP/main.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lect(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,8 +24,20 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Revicewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student,
+                      Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -27,15 +49,19 @@ class Mentor:
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_revicewer = Revicewer('Mike', 'Tyson')
+cool_revicewer.courses_attached += ['Python']
+
+cool_revicewer.rate_hw(best_student, 'Python', 10)
+cool_revicewer.rate_hw(best_student, 'Python', 10)
+cool_revicewer.rate_hw(best_student, 'Python', 10)
+
+best_student.rate_lect(cool_lecturer, 'Python', 10)
+best_student.rate_lect(cool_lecturer, 'Python', 9)
+best_student.rate_lect(cool_lecturer, 'Python', 8)
 
 print(best_student.grades)
-class Lecturer (Mentor):
-    pass
-class Reviewer (Mentor):
-    pass
+print(cool_lecturer.grades)
