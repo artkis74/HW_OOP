5762a5c6bf42fae004cf9aeff5973134dae93ea5
artkis74 <kiselev_artem1991@mail.ru>
2022-07-21 18:50:40 +0500

    added task 4

--- HW_OOP/main.py
+++ HW_OOP/main.py
@@ -21,17 +21,20 @@ class Student:
         for grades in self.grades.values():
             average_grade = round(sum(grades) / len(grades), 1)
         return average_grade
-    def __str__(self):
-        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСреднняя оценка за домашние задания: ' \
-              f'{self.average_grades()}\nКурсы в процессе изучения: {",".join(self.courses_in_progress)}\n'\
-              f'Завершенные курсы: {",".join(self.finished_courses)}'
-        return res
 
-    def __lt__(self, other):
-        if not isinstance(other, Student):
-            print('Not a Student!')
-            return
-        return self.average_grades() < other.average_grades()
+    def average_grades_for_course(students, course):
+        grade = 0
+        for student in students:
+             if course not in student.courses_in_progress:
+                 return 'This course is not finished'
+             elif not isinstance (student, Student):
+                 return (f'{student} is not student')
+             else:
+                grade +=  (sum(student.grades[course]) / len(student.grades[course]))  #Cкажите, пожалуйста
+            # можно ли здесь было воспользоваться (если можно то как) уже имеющимся методом average_grades,
+            # а не писать его логику заново?
+                average_grade = round(grade / len(students), 1)
+        return f'Средняя оценка студентов за курс {course}: {average_grade}'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -60,6 +63,19 @@ class Lecturer(Mentor):
             return
         return self.average_grades() < other.average_grades()
 
+    def average_grades_for_lecture(lecturers, course):
+        grade = 0
+        for lecturer in lecturers:
+             if course not in lecturer.courses_attached:
+                 return 'This lecturer is not attached for this course'
+             elif not isinstance (lecturer, Lecturer):
+                 return (f'{student} is not lecturer')
+             else:
+                grade +=  (sum(lecturer.grades[course]) / len(lecturer.grades[course]))  #Cкажите,
+                # пожалуйста можно ли здесь было воспользоваться (если можно то как) уже имеющимся
+                # методом average_grades, а не писать его логику заново?
+                average_grade = round(grade / len(lecturers), 1)
+        return f'Средняя оценка лекторов за курс {course}: {average_grade}'
 
 class Revicewer(Mentor):
     def __init__(self, name, surname):
@@ -82,10 +98,8 @@ class Revicewer(Mentor):
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
-best_student.finished_courses += ["Введение в программирование"]
-best_student1 = Student('Some', 'Student', 'your_gender')
-best_student1.courses_in_progress += ['Python', 'Git']
-best_student1.finished_courses += ["Введение в программирование"]
+best_student2 = Student('Bill', 'Epman', 'your_gender')
+best_student2.courses_in_progress += ['Python']
 
 cool_lecturer = Lecturer('Some', 'Buddy')
 cool_lecturer.courses_attached += ['Python']
@@ -95,19 +109,19 @@ cool_lecturer1.courses_attached += ['Python']
 cool_revicewer = Revicewer('Mike', 'Tyson')
 cool_revicewer.courses_attached += ['Python']
 
+cool_revicewer.rate_hw(best_student, 'Python', 1)
 cool_revicewer.rate_hw(best_student, 'Python', 10)
 cool_revicewer.rate_hw(best_student, 'Python', 10)
-cool_revicewer.rate_hw(best_student, 'Python', 10)
-cool_revicewer.rate_hw(best_student1, 'Python', 2)
-cool_revicewer.rate_hw(best_student1, 'Python', 5)
-cool_revicewer.rate_hw(best_student1, 'Python', 7)
+cool_revicewer.rate_hw(best_student2, 'Python', 10)
+cool_revicewer.rate_hw(best_student2, 'Python', 10)
+cool_revicewer.rate_hw(best_student2, 'Python', 10)
 
-best_student.rate_lect(cool_lecturer, 'Python', 1)
 best_student.rate_lect(cool_lecturer, 'Python', 10)
-best_student.rate_lect(cool_lecturer, 'Python', 8)
-best_student.rate_lect(cool_lecturer1, 'Python', 2)
-best_student.rate_lect(cool_lecturer1, 'Python', 4)
-best_student.rate_lect(cool_lecturer1, 'Python', 5)
+best_student.rate_lect(cool_lecturer, 'Python', 10)
+best_student.rate_lect(cool_lecturer, 'Python', 10)
+best_student.rate_lect(cool_lecturer1, 'Python', 10)
+best_student.rate_lect(cool_lecturer1, 'Python', 10)
+best_student.rate_lect(cool_lecturer1, 'Python', 10)
 
 print(best_student.grades)
 print(cool_lecturer.grades)
@@ -117,5 +131,5 @@ print(best_student.average_grades())
 print(cool_lecturer1.average_grades())
 print(cool_lecturer1)
 print(cool_lecturer1 < cool_lecturer)
-print(best_student1)
-print(best_student > best_student1)
+print(Student.average_grades_for_course([best_student, best_student2], 'Python'))
+print(Lecturer.average_grades_for_lecture([cool_lecturer, cool_lecturer1], 'Python'))
